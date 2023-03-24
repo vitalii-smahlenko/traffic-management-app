b6efa9cb8b41c6d0a6f8687fd337743e5e69680d
Vitalii Smahlenko <smaglenko@gmail.com>
2023-03-24 15:48:16 +0200

    Implemented PlaneCalculation

--- src/main/java/com/gmail/smaglenko/trafficmanagementapp/TrafficManagementAppApplication.java
+++ src/main/java/com/gmail/smaglenko/trafficmanagementapp/TrafficManagementAppApplication.java
@@ -7,13 +7,16 @@ import com.gmail.smaglenko.trafficmanagementapp.model.Flight;
 import com.gmail.smaglenko.trafficmanagementapp.model.TemporaryPoint;
 import com.gmail.smaglenko.trafficmanagementapp.model.WayPoint;
 import com.gmail.smaglenko.trafficmanagementapp.service.AircraftService;
+import com.gmail.smaglenko.trafficmanagementapp.service.PlaneCalculation;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TrafficManagementAppApplication {
 
     public static void main(String[] args) {
@@ -21,9 +24,42 @@ public class TrafficManagementAppApplication {
     }
 
     @Bean
-    CommandLineRunner run(AircraftService aircraftService) {
+    CommandLineRunner run(AircraftService aircraftService, PlaneCalculation planeCalculation) {
         return args -> {
+            Aircraft airplane1 = new Airplane();
 
+            AirplaneCharacteristics airplane1Characteristics = new AirplaneCharacteristics();
+            airplane1Characteristics.setTurnRate(200);
+            airplane1Characteristics.setMaxSpeed(150);
+            airplane1Characteristics.setMaxAcceleration(100);
+            airplane1Characteristics.setClimbRate(10);
+
+            WayPoint firstWayPointForAirplane1 = new WayPoint();
+            firstWayPointForAirplane1.setLatitude(30.514612);
+            firstWayPointForAirplane1.setLongitude(50.530600);
+            firstWayPointForAirplane1.setHeight(1000);
+            firstWayPointForAirplane1.setSpeed(120);
+
+            WayPoint secondWayPointForAirplane1 = new WayPoint();
+            secondWayPointForAirplane1.setLatitude(30.514605);
+            secondWayPointForAirplane1.setLongitude(50.515705);
+            secondWayPointForAirplane1.setHeight(1000);
+            secondWayPointForAirplane1.setSpeed(120);
+
+            WayPoint thirdWayPointForAirplane1 = new WayPoint();
+            thirdWayPointForAirplane1.setLatitude(30.500160);
+            thirdWayPointForAirplane1.setLongitude(50.516652);
+            thirdWayPointForAirplane1.setHeight(1000);
+            thirdWayPointForAirplane1.setSpeed(120);
+
+            List<WayPoint> wayPointsForAirplane1 = new ArrayList<>();
+            wayPointsForAirplane1.add()
+
+            planeCalculation.calculateRoute()
+
+            airplane1.setCharacteristics();
+            airplane1.setPosition();
+            airplane1.getFlights();
         };
     }
 
--- src/main/java/com/gmail/smaglenko/trafficmanagementapp/service/impl/PlaneCalculationImpl.java
+++ src/main/java/com/gmail/smaglenko/trafficmanagementapp/service/impl/PlaneCalculationImpl.java
@@ -6,18 +6,122 @@ import com.gmail.smaglenko.trafficmanagementapp.model.WayPoint;
 import com.gmail.smaglenko.trafficmanagementapp.service.PlaneCalculation;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class PlaneCalculationImpl implements PlaneCalculation {
-
-    @Override
     public List<TemporaryPoint> calculateRoute(AirplaneCharacteristics characteristics,
                                                List<WayPoint> wayPoints) {
+        // Ініціалізація тимчасових змінних
         List<TemporaryPoint> temporaryPoints = new ArrayList<>();
-
+        TemporaryPoint currentPosition = new TemporaryPoint(wayPoints.get(0).getLatitude(),
+                wayPoints.get(0).getLongitude(), wayPoints.get(0).getHeight(), 0, 0);
+        double currentSpeed = 0;
+        double currentCourse = 0;
+        double timeInterval = 1;
+        double earthRadius = 6371.0; // Радіус Землі в км
+        // Обробка кожної точки маршруту
+        for (int i = 1; i < wayPoints.size(); i++) {
+            WayPoint currentWayPoint = wayPoints.get(i);
+            WayPoint previousWayPoint = wayPoints.get(i - 1);
+            double distance = calculateDistance(currentPosition.getLatitude(),
+                    currentPosition.getLongitude(), currentWayPoint.getLatitude(),
+                    currentWayPoint.getLongitude(), earthRadius);
+            double time = distance / currentSpeed;
+            double requiredAltitude = currentWayPoint.getHeight();
+            double requiredSpeed = currentWayPoint.getSpeed();
+            double requiredCourse = calculateCourse(currentPosition.getLatitude(),
+                    currentPosition.getLongitude(), currentWayPoint.getLatitude(),
+                    currentWayPoint.getLongitude());
+            double acceleration = characteristics.getMaxAcceleration();
+            double maxSpeed = characteristics.getMaxSpeed();
+            // Обчислення максимальної швидкості, яку може розвинути літак на цій ділянці маршруту
+            double maxPossibleSpeed = Math.min(maxSpeed, Math.sqrt(currentSpeed * currentSpeed
+                    + 2 * acceleration * distance));
+            // Перевірка можливості розвинути необхідну швидкість на даній ділянці маршруту
+            if (requiredSpeed > maxPossibleSpeed) {
+                // Якщо швидкість недостатня, потрібно прискоритися
+                while (currentSpeed < requiredSpeed) {
+                    currentSpeed += acceleration * timeInterval;
+                    currentSpeed = Math.min(currentSpeed, maxPossibleSpeed);
+                    double currentAltitude = currentPosition.getHeight();
+                    double altitudeChange = requiredAltitude - currentAltitude;
+                    double altitudeSpeed = characteristics.getMaxSpeed();
+                    currentPosition = move(currentPosition, requiredCourse, currentSpeed,
+                            altitudeSpeed, altitudeChange, timeInterval, earthRadius);
+                    temporaryPoints.add(currentPosition);
+                }
+            } else {
+                // Якщо швидкість достатня, потрібно підтримувати її
+                currentSpeed = requiredSpeed;
+                double currentAltitude = currentPosition.getHeight();
+                double altitudeChange = requiredAltitude - currentAltitude;
+                double altitudeSpeed = characteristics.getMaxSpeed();
+                currentPosition = move(currentPosition, requiredCourse, currentSpeed, altitudeSpeed,
+                        altitudeChange, time, earthRadius);
+                temporaryPoints.add(currentPosition);
+            }
+        }
         return temporaryPoints;
+    }
+
+    private double calculateDistance(double startLat, double startLon, double endLat, double endLon,
+                                     double earthRadius) {
+        // Переведення координат у радіани
+        double lat1 = Math.toRadians(startLat);
+        double lon1 = Math.toRadians(startLon);
+        double lat2 = Math.toRadians(endLat);
+        double lon2 = Math.toRadians(endLon);
+        // Обчислення різниці координат
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+        // Обчислення відстані між точками за формулою гаверсинусів
+        double a = Math.pow(Math.sin(dLat / 2), 2) +
+                Math.cos(lat1) * Math.cos(lat2) *
+                        Math.pow(Math.sin(dLon / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return earthRadius * c;
+    }
+
+    private double calculateCourse(double startLat, double startLon, double endLat, double endLon) {
+        // Переведення координат у радіани
+        double lat1 = Math.toRadians(startLat);
+        double lon1 = Math.toRadians(startLon);
+        double lat2 = Math.toRadians(endLat);
+        double lon2 = Math.toRadians(endLon);
+        // Обчислення різниці координат
+        double dLon = lon2 - lon1;
+        // Обчислення курсу
+        double y = Math.sin(dLon) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2) -
+                Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+        double course = Math.toDegrees(Math.atan2(y, x));
+        course = (course + 360) % 360;
+        return course;
+    }
+
+    private TemporaryPoint move(TemporaryPoint currentPosition, double course, double speed,
+                                double altitudeSpeed, double altitudeChange,
+                                double timeInterval, double earthRadius) {
+        // Переведення курсу в радіани
+        double radianCourse = Math.toRadians(course);
+        // Обчислення зміни висоти за час timeInterval
+        double altitudeChangePerInterval = altitudeSpeed * timeInterval;
+        double newHeight = currentPosition.getHeight() + altitudeChange;
+        double climbRate = altitudeChange / timeInterval;
+        // Обчислення переміщення вздовж широти та довготи
+        double distance = speed * timeInterval;
+        double lat1 = Math.toRadians(currentPosition.getLatitude());
+        double lon1 = Math.toRadians(currentPosition.getLongitude());
+        double angularDistance = distance / earthRadius;
+        double lat2 = Math.asin(Math.sin(lat1) * Math.cos(angularDistance) +
+                Math.cos(lat1) * Math.sin(angularDistance) * Math.cos(radianCourse));
+        double lon2 = lon1 + Math.atan2(Math.sin(radianCourse) * Math.sin(angularDistance)
+                        * Math.cos(lat1),
+                Math.cos(angularDistance) - Math.sin(lat1) * Math.sin(lat2));
+        // Переведення координат назад у градуси
+        double newLatitude = Math.toDegrees(lat2);
+        double newLongitude = Math.toDegrees(lon2);
+        return new TemporaryPoint(newLatitude, newLongitude, newHeight, climbRate, speed);
     }
 }
